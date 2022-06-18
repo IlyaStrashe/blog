18439dbcc786f9d97c059b5eebf6ac41e2f69a54
IlyaStrashe <105169119+IlyaStrashe@users.noreply.github.com>
2022-06-18 20:38:37 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+// Custom Scripts
+// Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active-burger')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+    const nav = document.querySelector('nav')
+
+    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+    const breakpoint = 1
+    if (window.scrollY >= breakpoint) {
+        nav.classList.add('fixed__nav')
+    } else {
+        nav.classList.remove('fixed__nav')
+    }
+}
+window.addEventListener('scroll', fixedNav)
+
+
+
+
+
+
+
+const list = document.querySelector('.list'),
+    items = document.querySelectorAll('.blocks__item')
+listItems = document.querySelectorAll('.list__item')
+
+function filter() {
+    list.addEventListener('click', event => {
+        const targetId = event.target.dataset.id
+        const target = event.target
+
+        if (target.classList.contains('list__item')) {
+            listItems.forEach(listItem => listItem.classList.remove('active'))
+            target.classList.add('active')
+        }
+
+
+        switch (targetId) {
+            case 'all':
+                getItems('blocks__item')
+                break
+            case 'winter':
+                getItems(targetId)
+                break
+            case 'spring':
+                getItems(targetId)
+                break
+            case 'autumn':
+                getItems(targetId)
+                break
+            case 'summer':
+                getItems(targetId)
+                break
+        }
+    })
+}
+filter()
+
+function getItems(className) {
+    items.forEach(item => {
+        if (item.classList.contains(className)) {
+            item.style.display = 'block'
+        } else {
+            item.style.display = 'none'
+        }
+    })
+}
